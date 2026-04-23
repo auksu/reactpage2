9d165b29c94396840704b84d6f38489eb4cc31ee
auksu <newmail@users.noreply.github.com>
2026-04-23 18:15:53 +0900

    update all source

--- src/App.jsx
+++ src/App.jsx
@@ -1,220 +1,186 @@
 import { useState } from 'react';
+import { Routes, Route, NavLink } from 'react-router-dom';
 import reactLogo from './assets/dots.svg';
-import raizan from './image/raizan.png';
+import ua from './image/ua.jpg';
+import github from './image/github.png';
+import x from './image/x.png';
+import insta from './image/insta.png';
 import tTherays from './image/t-therays.png';
+import linkIcon from './image/link.png'; // なければ後でOK
 import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0);
-
+function Home({ setPage }) {
   return (
-    <>
-      <div id ="home">
+    <div className="home-page">
+      <div id="home">
         <a href="https://vite.dev" target="_blank" rel="noreferrer"></a>
         <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img
             src={reactLogo}
             className="logo react"
             alt="React logo"
-            style={{ width: '510px', height: 'auto' }}
+            style={{ width: '40%', height: 'auto' }}
           />
         </a>
       </div>
-      <h1>内田 朝陽</h1>
-      <div className="card">
-        <h2><a href="#hoge">Learn Asahi</a></h2>
+
+      <h2>
+        <span
+          style={{ cursor: 'pointer', color: '#646cff' }}
+          onClick={() => setPage('about')}
+        >
+          Learn Asahi
+        </span>
+      </h2>
+
+      <p>
+        <code>Node.js</code>と<code>React</code>で作って
+        <code>GitHub</code>でデプロイしました.
+      </p>
+    </div>
+  );
+}
+
+function About() { 
+  return (
+    <>
+      <h1>About・Profile</h1>
+      <div className="about-container">
+        <div className="about-image">
+          <img alt="雷" src={ua} />
+        </div>
+
+        <div className="about-text">
+          <h1><span style={{ fontSize: '50px' }}>内田 朝陽</span> <br></br> Uchida Asahi</h1>
+          <h2>出身：熊本</h2>
+          <h3>学部：情理(2回~) 経済(1回)<br></br>年齢：28卒</h3>
+          <p>京都産業大学 情報理工学部の内田朝陽です。主にWeb系に興味があり、React・Node.js、CSSなどを使ってバイブコーディングを行なっています。</p>
+          <p>あまてくというエンジニアコミュニティの運営に携わっており、ハッカソンの開催やプロダクト制作の企画や運営を行っています。<br></br>他にC.A.C.というデジタル創作系の部活や登山部に所属し活動しています。</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+function Projects() {
+  return (
+    <>
+      <h1>Projects</h1>
+      <div className="project-p">
+        <h2>プロジェクト</h2>
+        <p>今までに取り組んだプロジェクトの一部を紹介します。</p>
+      </div>
+
+      <div className="projects-grid">
+        {/* 1個目 */}
+        <div className="project-card">
+          <img src={ua} alt="project1" />
+          <h2>ポートフォリオサイト</h2>
+          <p>React・Node.js、CSSで作成しGithubにデプロイした個人ポートフォリオサイト。ReactによりSPA化しています。レスポンシブデザインに対応する予定。</p>
+          <div className="project-links">
+            <a href="https://auksu.github.io/reactpage2/" target="_blank" rel="noreferrer">
+              <img src={linkIcon} alt="site" 
+              style={{ width: '15px', height: 'auto' }}
+              />
+              Site
+            </a>
+            <p></p>
+            <a href="https://github.com/auksu/reactpage2" target="_blank" rel="noreferrer">
+              <img src={github} alt="github" style={{ width: '15px', height: 'auto' }}
+              />
+              GitHub
+            </a>
+          </div>
+        </div>
+
+        {/* 2個目 */}
+        <div className="project-card">
+          <img src={github} alt="project2" />
+          <h2>部内向けサンプルページ</h2>
+          <p>React・Node.js、CSSを部員が学ぶために作成した基本的なページ</p>
+                    <div className="project-links">
+            <a href="https://auksu.github.io/food_react/" target="_blank" rel="noreferrer">
+              <img src={linkIcon} alt="site" 
+              style={{ width: '15px', height: 'auto' }}
+              />
+              Site
+            </a>
+            <p></p>
+            <a href="https://github.com/auksu/food_react" target="_blank" rel="noreferrer">
+              <img src={github} alt="github" style={{ width: '15px', height: 'auto' }}
+              />
+              GitHub
+            </a>
+          </div>
+        </div>
+
+        {/* 追加していけばOK */}
+      </div>
+      <div className='icons8'>
         <p>
-<<<<<<< HEAD
-          <code>Node.js</code>と<code>React</code>で作って<code>GitHub</code>でデプロイしてみました。
-=======
-          <code>Node.js</code>と<code>React</code>で作って<code>AWS</code>でデプロイしてみました。
->>>>>>> 2ed4f31c49dd2466016a95885498bf7f242a5e45
+          <br></br><a target="_blank" href="https://icons8.com/icon/83168/external-link">外部リンク</a> アイコン by <a target="_blank" href="https://icons8.com">Icons8</a>
         </p>
       </div>
+    </>
+  );
+}
 
-      <div className="card">
-      <div id = "hoge" className="affiliation">
-        <div className="myphoto">
-          <a href="https://x.com/talesof_therays/status/1505469201884057600/photo/1">
-            <img alt="襲爪雷斬" width="100" src={raizan} />
-          </a>
-        </div>
-        <div className="myinfo">
-          <h2>出身：熊本</h2>
-          <h3>学部：情理(2回~) 経済(1回)</h3>
-        </div>
-      </div>
-
-      <div className="contentArea_white">
-        <div className="box">
-          <div className="title">
-            <h2 className="titleSentence">テイルズオブシリーズ</h2>
-          </div>
-          <p className="contentInfo">
-            <strong>
-              デスティニー、ヴェスペリア、ザレイズが一番好きです。
-              <br />
-              ザレイズをサ終させたバンダイナムコを許しません
-            </strong>
-          </p>
+function LinksPage() { 
+  return (
+    <>
+      <div id = "hoge" className="myphoto">
+        <a href="https://github.com/auksu">
+          <img alt="github" width="100" src={github} />
+        </a>
+        <a href="https://x.com/ua_Rai_">
+          <img alt="x" width="100" src={x} />
+        </a>
+        <a href="https://www.instagram.com/auuauaau1887/">
+          <img alt="insta" width="100" src={insta} />
+        </a>
+        <div className='icons8'>
+          <p><a target="_blank" href="https://icons8.com/icon/12599/github">GitHub</a> アイコン by <a target="_blank" href="https://icons8.com">Icons8</a></p>
+          <p><a target="_blank" href="https://icons8.com/icon/phOKFKYpe00C/x">ツイッターx</a> アイコン by <a target="_blank" href="https://icons8.com">Icons8</a></p>
+          <p><a target="_blank" href="https://icons8.com/icon/59813/instagram">インスタグラム</a> アイコン by <a target="_blank" href="https://icons8.com">Icons8</a></p>
         </div>
       </div>
+    </>
+  );
+}
 
-      <div className="contentArea_color">
-        <div className="box">
-          <div className="title">
-            <h2 className="titleSentence">好きなこと</h2>
-          </div>
-          <p className="contentInfo">
-            <strong>
-              山登ったりしてます。サイクリングもしてます。
-              <br />
-              他に野球やイナズマイレブンなどプレイしてます。
-              <br />
-              たまにポケモン、スマブラ、マイクラ、FF4、GV、その他JRPG
-              <br />
-              などやったりしています。
-            </strong>
-          </p>
+function App() {
+  const [page, setPage] = useState('home');
+
+  return (
+    <>
+      <nav className="navbar">
+        <div className="nav-left">
+          <span onClick={() => setPage('home')}>
+            <img
+              src={reactLogo}
+              className="logo react"
+              alt="React logo"
+              style={{ width: '30%', height: 'auto' }}
+            />
+          </span>
         </div>
-      </div>
 
-      <div className="contentArea_white">
-        <div className="box">
-          <div className="title">
-            <h2 className="titleSentence">好きなアニメ</h2>
-          </div>
-          <p className="contentInfo">
-            <strong>
-              イナイレ、テイルズ、MIX
-              <br />
-              スレイヤーズ、ゆるキャン△、ヤマノススメ
-            </strong>
-          </p>
+        <div className="nav-right">
+          <span onClick={() => setPage('about')}>About・Profile</span>
+          <span onClick={() => setPage('projects')}>Projects</span>
+          <span onClick={() => setPage('links')}>LINKS</span>
         </div>
-      </div>
+      </nav>
 
-      <div className="contentArea_color">
-        <div className="box">
-          <div className="title">
-            <h2 className="titleSentence">生息地</h2>
-          </div>
-          <table border="3" align="center" width="50%">
-            <tbody>
-              <tr>
-                <th>
-                  <div className="seisokuchibar">エリア</div>
-                </th>
-                <th>
-                  <div className="seisokuchibar">出現率</div>
-                </th>
-              </tr>
-              <tr>
-                <th>
-<<<<<<< HEAD
-                  <div className="seisokuchi">木曜AS1・金曜AM</div>
-=======
-                  <div className="seisokuchi">月曜PM・金曜AS1</div>
->>>>>>> 2ed4f31c49dd2466016a95885498bf7f242a5e45
-                </th>
-                <th><font color="black">シンボル</font></th>
-              </tr>
-              <tr>
-                <th>
-                  <div className="seisokuchi">14号館(情理)</div>
-                </th>
-                <th>
-                  <font color="black">◎</font><span style={{ color: 'grey' }}>[朝昼夕]</span>
-                </th>
-              </tr>
-              <tr>
-                <th>
-                  <div className="seisokuchi">いえ・ぶしつ</div>
-                </th>
-                <th><font color="black">○</font></th>
-              </tr>
-              <tr>
-                <th>
-                  <div className="seisokuchi">そと</div>
-                </th>
-                <th>
-                  <font color="black">△</font><span style={{ color: 'grey' }}>[晴]</span>
-                </th>
-              </tr>
-              <tr>
-                <th>
-                  <div className="seisokuchi">やま</div>
-                </th>
-                <th>
-                  <font color="black">▲</font><span style={{ color: 'grey' }}>[晴]</span>
-                </th>
-              </tr>
-            </tbody>
-          </table>
-          <br></br>
-        </div>
-      </div>
-
-      <div className="contentArea_white">
-        <div className="box">
-          <div className="title">
-            <h2 className="titleSentence">部活・サークル等</h2>
-          </div>
-          <p className="contentInfo">
-<<<<<<< HEAD
-            <strong>C.A.C.・<s>軟式野球サークル</s>・探検部・あまてく</strong>
-=======
-            <strong>C.A.C.・<s>軟式野球サークル</s>・あまてく・探検部</strong>
->>>>>>> 2ed4f31c49dd2466016a95885498bf7f242a5e45
-          </p>
-        </div>
-      </div>
-
-      <div className="contentArea_color">
-        <div className="box">
-          <div className="title">
-            <h2 className="titleSentence">今年度の目標</h2>
-          </div>
-          <p className="contentInfo">
-            <strong>
-              GPA3以上、なんか作る、なんか習得する
-              <br />
-<<<<<<< HEAD
-              夏の<a href="https://www.city.maibara.lg.jp/mtibuki/about_tozan/index.html">伊吹山</a>・六甲山・金剛山、雪の金剛山・蓬莱山(行った)・伊吹山
-              <br />
-              10キロマラソン・<a href="https://www.biwako1.jp/basic/plan">ビワイチ</a>(達成*2)・<a href="https://route3776.jp">富士3776</a>
-              <br />
-              動画制作、インターン(行った)、野球
-=======
-              夏の<a href="https://www.city.maibara.lg.jp/mtibuki/about_tozan/index.html">伊吹山</a>・六甲山・金剛山、雪の金剛山・蓬莱山・伊吹山
-              <br />
-              10キロマラソン・<a href="https://www.biwako1.jp/basic/plan">ビワイチ</a>(達成)・<a href="https://route3776.jp">富士3776</a>
-              <br />
-              動画制作、インターン、野球
->>>>>>> 2ed4f31c49dd2466016a95885498bf7f242a5e45
-            </strong>
-          </p>
-        </div>
-      </div>
-      <div className="contentArea_white">
-      <div className="center">
-        <a href="https://tales-ch.jp/special_index.php?ms_id=227"><img alt="ザレイズありがとう" height="400" src={tTherays} /></a>
-      </div>
-      </div>  
-
-      <div class="card">
-      <div className="meiki">
-        <p>author : Uchida Asahi</p>
-<<<<<<< HEAD
-        <p>version : 1.0.3</p>
-        <p>date : 2025/10/02 (created: 2025/06/20)</p>
-=======
-        <p>version : 1.0.2</p>
-        <p>date : 2025/07/01 (created: 2025/06/20)</p>
->>>>>>> 2ed4f31c49dd2466016a95885498bf7f242a5e45
-      </div>
-      </div>
-      </div>
+      <main
+        key={page}
+        className={`${page === 'home' ? 'home-bg' : 'white-bg'} fade`}
+        >
+        {page === 'home' && <Home setPage={setPage} />}
+        {page === 'about' && <About />}
+        {page === 'projects' && <Projects />}
+        {page === 'links' && <LinksPage />}
+      </main>
     </>
   );
 }
